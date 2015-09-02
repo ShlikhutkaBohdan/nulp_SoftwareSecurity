4ca325c02ffe5934d073cacf7de4e61e00523674
ShlikhutkaBohdan <bohdan170194@gmail.com>
2015-09-02 14:20:56 +0300

    md5 class with basic func (only test)

---
+++ lab2_md5/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2_md5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MyMd5.md5test();
+        }
+    }
+}
